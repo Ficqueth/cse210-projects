9cc7a571887ef4e5cb95c4d431bc8927d199e4a8
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-01-10 08:24:45 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is grade percentage? ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string letter = "";
+
+        if (percentage >= 90 )
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"{letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed the course, congratulation!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, with some more study, i'm sure you'll pass next time");
+        }
     }
 }
