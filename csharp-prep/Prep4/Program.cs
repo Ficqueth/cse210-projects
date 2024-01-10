97cec8c3d906f9ee35234f87026c9e2eb742a715
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-01-10 12:47:58 -0600

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int numberInput = -1;
+        Console.WriteLine("Enter a list of numbers, type '0' when finished. ");
+        while (numberInput != 0)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            numberInput = int.Parse(input);
+            if (numberInput != 0)
+            {
+                numbers.Add(numberInput);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+            Console.WriteLine($"The sum is: {sum}");
+        
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int largest = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+            Console.WriteLine($"The largest number is : {largest}");
     }
 }
