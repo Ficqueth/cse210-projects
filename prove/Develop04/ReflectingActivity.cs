34688397601e50e3b7e0687b25169fd7ea80e984
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-01-31 15:02:38 -0600

    Develop04 , skeleton

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+public class ReflectingActivity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+
+    public ReflectingActivity()
+    {
+
+    }
+
+    public void Run()
+    {
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return _prompts[0];
+    }
+
+    public string GetRandomQuestion()
+    {
+        return _questions[0];
+    }
+
+    public void DisplayPrompt()
+    {
+
+    }
+
+    public void DisplayQuestions()
+    {
+        
+    }
+}
