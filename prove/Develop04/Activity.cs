d78aedf8daea64e29c7e4da74bcc80df605ddae9
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-02-01 09:57:55 -0600

    working spinner

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -13,17 +13,32 @@ public class Activity
 
     public void DisplayStartingMessage()
     {
-
+        Console.WriteLine($"Welcome to the {_name}.\n");
+        Console.WriteLine($"{_description}");
     }
 
     public void DisplayEndingMessage()
     {
-
+        Console.WriteLine($"Well done!!\n\nYou have completed another {_duration} of the {_name}");
     }
 
     public void ShowSpinner(int seconds)
     {
-
+        for (int i = seconds; i> 0; i--)
+        {
+        Console.Write("-");
+        Thread.Sleep(250);
+        Console.Write("\b \b");
+        Console.Write("\\");
+        Thread.Sleep(250);
+        Console.Write("\b \b");
+        Console.Write("|");
+        Thread.Sleep(250);
+        Console.Write("\b \b");
+        Console.Write("/");
+        Thread.Sleep(250);
+        Console.Write("\b \b");
+        }
     }
 
     public void ShowCountDown(int seconds)
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+
+        Activity breathing = new Activity();
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1. Start breathing activity");
+        Console.WriteLine("2. Start reflecting activity");
+        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("4. Quit");
+
+        Console.Write("\nSelect a choice from the menu: ");
+
+        int choice;
+        if (int.TryParse(Console.ReadLine(), out choice))
+        {
+            switch(choice)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the Breathing Activity.\n");
+                    Console.WriteLine("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.");
+                    Console.WriteLine("\nHow long, in seconds, would you like for your session? ");
+                    int.TryParse(Console.ReadLine(), out int seconds);
+                    breathing.ShowSpinner(seconds);
+
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+            }
+        }
+        Console.WriteLine("Exiting program.");
     }
 }
