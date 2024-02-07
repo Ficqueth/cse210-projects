b676867684a4df43103f5140f795fbe3921fc298
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-02-07 17:01:36 -0600

    week5 learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -16,5 +16,17 @@ class Program
         Console.WriteLine(c1.GetColor());
         Console.WriteLine(c1.GetArea());
 
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(c1);
+        shapes.Add(s1);
+        shapes.Add(r1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
+
     }
 }
