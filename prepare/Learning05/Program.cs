272fdbadfef46cab5a41c603a0fdf96c6e64ae14
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-02-07 16:51:18 -0600

    WEEK5 learning

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square s1 = new Square("blue", 5);
+        Console.WriteLine(s1.GetColor());
+        Console.WriteLine(s1.GetArea());
+
+        Rectangle r1 = new Rectangle("red", 3, 7);
+        Console.WriteLine(r1.GetColor());
+        Console.WriteLine(r1.GetArea());
+
+        Circle c1 = new Circle("green", 3);
+        Console.WriteLine(c1.GetColor());
+        Console.WriteLine(c1.GetArea());
+
     }
 }
