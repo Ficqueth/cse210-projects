3a10ab880ce22225983a005cb787f48eedbb0856
“Helaman_Ficquet” <helaman.ficquet@cdw.com>
2024-02-10 09:05:02 -0600

    working

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,24 +1,52 @@
+using System;
+
 public class EternalGoal : Goal
 {
+    private int _stepCounter;
 
-    public EternalGoal(string name, string description, string points) : base(name, description, points)
+    public EternalGoal()
     {
-        
+        _name = "";
+        _description = "";
+        _goalPoints = 50;
+        _stepCounter = 0;
+    }
+    public EternalGoal(string name, string description, int goalPoints)
+    {
+        _name = name;
+        _description = description;
+        _goalPoints = goalPoints;
+        _stepCounter = 0;
+    }
+    public override void CreateChildGoal()
+    {
+        CreateBaseGoal();
     }
 
-    public override void RecordEvent()
+    public override void ListGoal()
     {
-
+        Console.Write($"[ ] {_name} ({_description})");
     }
 
     public override bool IsComplete()
     {
-        return true;
+        return false;
     }
 
-    public override string GetStringRepresentation()
+    public override string SaveGoal()
     {
-        return "";
+        string line = "";
+        line = $"EternalGoal:" + _name + "," + _description + "," + _goalPoints.ToString();
+        return line;
     }
 
+    public override void RecordEvent()
+    {
+        _stepCounter ++;
+    }
+    public override int CalculateAGP()
+    {
+        int points = _goalPoints;
+        return points;
+    }
 }
--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -1,25 +1,74 @@
+using System;
+
 public class SimpleGoal : Goal
 {
-    private bool _isComplete;
-
-    public SimpleGoal(string name, string description, string points) : base(name, description, points)
+    public SimpleGoal()
     {
+        _name = "Name of Simple Goal";
+        _description = "Description of Simple Goal";
+        _goalPoints = 50;
+        _status = false;
+    }
 
+    public SimpleGoal(string name, string description, int goalPoints, bool status)
+    {
+        _name = name;
+        _description = description;
+        _goalPoints = goalPoints;
+        _status = status;
+    }
+
+    public override void CreateChildGoal()
+    {
+        CreateBaseGoal();
     }
 
     public override void RecordEvent()
     {
-        return ;
+        if (_status == false) {
+            _status = true;
+        } else {
+            Console.WriteLine("You have already completed this goal.");
+        }
     }
 
     public override bool IsComplete()
     {
-        return true;
+        if (_status == true) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public override string GetStringRepresentation()
+    public override void ListGoal()
     {
-        return "";
+        string statusSymbol = "";
+        bool status = IsComplete();
+        if (status == true) {
+            statusSymbol = "X";
+        } else {
+            statusSymbol = " ";
+        }
+        Console.Write($"[{statusSymbol}] {_name} ({_description})");
     }
     
+    public override int CalculateAGP()
+    {
+        bool status = IsComplete();
+        int aGP = 0;
+        if (status == true) {
+            aGP = _goalPoints;
+        } else {
+            aGP = 0;
+        }
+        return aGP;
+    }
+
+    public override string SaveGoal()
+    {
+        string line = "";
+        line = $"SimpleGoal:{_name},{_description}{_goalPoints},{IsComplete().ToString()}";
+        return line;
+    }
 }
